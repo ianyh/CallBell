fca1ec7c5ea1f959ef1d2ce8df0ca2fe65515834
Ian Ynda-Hummel <ianynda@gmail.com>
2019-09-01 22:12:54 -0400

    Run a pr request monitor that updates status image

--- CallBell/AppDelegate.swift
+++ CallBell/AppDelegate.swift
@@ -13,18 +13,38 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     private var statusItem: NSStatusItem?
     @IBOutlet var statusMenu: NSMenu?
     @IBOutlet var versionMenuItem: NSMenuItem?
+    
+    private lazy var monitor: ReviewRequestsMonitor = {
+        return ReviewRequestsMonitor(username: "ianyh", token: "<>") { hasReviewRequests in
+            self.updateStatusImage(isEnabled: hasReviewRequests)
+        }
+    }()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        let statusItemImage = NSImage(named: "status")
-        statusItemImage?.isTemplate = true
-
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        statusItem?.button?.image = statusItemImage
         statusItem?.menu = statusMenu
         
         let version = Bundle.main.infoDictionary?["CFBundleVersion"] as! String
         let shortVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String
         versionMenuItem?.title = "Version \(shortVersion) (\(version))"
+        
+        DispatchQueue.main.async {
+            self.monitor.start()
+        }
+    }
+    
+    private func updateStatusImage(isEnabled: Bool) {
+        let statusItemImage: NSImage?
+
+        if isEnabled {
+            statusItemImage = NSImage(named: "status")
+        } else {
+            statusItemImage = NSImage(named: "status-disabled")
+        }
+        
+        statusItemImage?.isTemplate = true
+
+        statusItem?.button?.image = statusItemImage
     }
     
     @IBAction func openReviewRequests(sender: Any) {
